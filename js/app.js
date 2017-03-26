e13c7059739f6e18ac18c289ec29c9242ad6d869
harinderkaur <kaurh1013@gmil.com>
2017-03-26 15:45:31 -0400

    java script

---
+++ js/app.js
@@ -0,0 +1,88 @@
+//1
+// var str = ("3" + 5 + 6)
+// var a = "3";
+// var b = 5;
+// var c = 6;
+// for (var i = 0; i < str.length; i++) {
+//     console.log(a + b + c);
+// }
+//or
+// console.log(str);
+//2
+var str = (3 + "5" + 6)
+
+var a = 3;
+var b = "5";
+var c = 6;
+
+for (var i = 0; i < str.length; i++) {
+    console.log(a + b + c);
+}
+// //3
+// 
+// var str = (3 + 5 + "6")
+// var a = 3;
+// var b = 5;
+// var c = "6";
+// for (var i = 0; i < str.length; i++) {
+
+//     console.log(a + b + c);
+// }
+// //4
+// var myString = "Pneumonoultramicroscopicsilicovolcanoconiosis";
+
+// for (var i = 0; i < myString.length; i++) {
+//     console.log(myString.length);
+
+// }
+//     
+// 5
+// var myString = ("Pneumonoultramicroscopicsilicovolcanoconiosis");
+// var newString = myString.length;
+// var myString = myString.toLowerCase
+// console.log("Pneumonoultramicroscopicsilicovolcanoconiosis".toLowerCase());
+
+//6
+// var myString = "Pneumonoultramicroscopicsilicovolcanoconiosis";
+// var newString = myString.length;
+// var myString = myString.toUpperCase
+// console.log("Pneumonoultramicroscopicsilicovolcanoconiosis".toUpperCase());
+
+
+// 
+// // //7
+
+// var myString = "I like this product";
+// "I hate this product";
+// "This product is worth buying";
+// "I don’ t agree with the above user";
+// var mySplitResult = myString.split("");
+// for (i = 0; i < mySplitResult.length; i++) {
+//     console.log(mySplitResult);
+// }
+//8
+// var myString = "I like this product I hate this product This product is worth buyingI don’ t agree with the above user";
+// var num_matches = myString.match(/o/gi).length;
+// console.log(num_matches);
+
+//example
+// var myString = 'Hello World. How are you doing?';
+// var splits = myString.split(' ', 3);
+
+// console.log(splits);
+
+//9
+// var myString = " Javascript notation %^&* notes that structure rules and not context for nothing";
+//  console.log(myString.match(/not/gi).length);
+
+//10 a
+
+// var myString = "I have to study Javascript"
+//      var res = myString.replace(/Javascript/g, "html");
+// console.log(res);
+
+//10b
+
+// var arr = ["Pneumonoultramicroscopicsilicovolcanoconiosis"];
+// arr = arr.map(i => "not" + i);
+// console.log(arr);
